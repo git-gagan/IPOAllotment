5c0f64fad5a82919b6cbf7952dd22014bdf48a66
root <root@LAPTOP-KOQKL6FT.localdomain>
2022-11-30 18:30:45 +0530

    initLedger and directory structure added for IPO-Settlement

---
+++ fabric-samples/ipo-settlement/javascript-chaincode/lib/ipo.js
@@ -0,0 +1,96 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { Contract } = require('fabric-contract-api');
+
+class Ipo extends Contract {
+
+    // Ipo class for shares settlement
+
+    async initLedger(ctx) {
+        console.info('============= START : Initialize Shares Ledger ===========');
+        const shares = [
+            // only one company is going for an IPO
+            {
+                quantity: 500,
+                company: 'Microsoft',
+                lotSize: 50,
+                priceRangeLow : 100,
+                priceRangeHigh : 200,
+                bidTime: 600 //seconds 
+            },
+        ];
+
+        for (let i = 0; i < shares.length; i++) {
+            shares[i].docType = 'share';
+            await ctx.stub.putState('SHARE' + i, Buffer.from(JSON.stringify(shares[i])));
+            console.info('Added <--> ', shares[i]);
+        }
+        console.info('============= END : Initialize Shares Ledger ===========');
+    }
+
+    async queryCar(ctx, carNumber) {
+        const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
+        if (!carAsBytes || carAsBytes.length === 0) {
+            throw new Error(`${carNumber} does not exist`);
+        }
+        console.log(carAsBytes.toString());
+        return carAsBytes.toString();
+    }
+
+    async createCar(ctx, carNumber, make, model, color, owner) {
+        console.info('============= START : Create Car ===========');
+
+        const car = {
+            color,
+            docType: 'car',
+            make,
+            model,
+            owner,
+        };
+
+        await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
+        console.info('============= END : Create Car ===========');
+    }
+
+    async queryAllCars(ctx) {
+        const startKey = '';
+        const endKey = '';
+        const allResults = [];
+        for await (const {key, value} of ctx.stub.getStateByRange(startKey, endKey)) {
+            const strValue = Buffer.from(value).toString('utf8');
+            let record;
+            try {
+                record = JSON.parse(strValue);
+            } catch (err) {
+                console.log(err);
+                record = strValue;
+            }
+            allResults.push({ Key: key, Record: record });
+        }
+        console.info(allResults);
+        return JSON.stringify(allResults);
+    }
+
+    async changeCarOwner(ctx, carNumber, newOwner) {
+        console.info('============= START : changeCarOwner ===========');
+
+        const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
+        if (!carAsBytes || carAsBytes.length === 0) {
+            throw new Error(`${carNumber} does not exist`);
+        }
+        const car = JSON.parse(carAsBytes.toString());
+        car.owner = newOwner;
+
+        await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
+        console.info('============= END : changeCarOwner ===========');
+    }
+
+}
+
+module.exports = Ipo; //WARNING
