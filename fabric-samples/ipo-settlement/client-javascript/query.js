6af3ebb44edc7ee406def7b88ad1a609700cce06
root <root@LAPTOP-KOQKL6FT.localdomain>
2022-12-02 10:51:01 +0530

    down script added | config in scripts | updated query.js

--- fabric-samples/ipo-settlement/client-javascript/query.js
+++ fabric-samples/ipo-settlement/client-javascript/query.js
@@ -13,6 +13,8 @@ const fs = require('fs');
 
 async function main() {
     try {
+        console.log(process.argv);
+        const userName = "user-" + process.argv[2];   // Take username from command line
         // load the network configuration
         const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
         console.log("==========================================\n", ccpPath)
@@ -24,19 +26,20 @@ async function main() {
         console.log(`Wallet path: ${walletPath}`);
 
         // Check to see if we've already enrolled the user.
-        const identity = await wallet.get('appUser');
+        const identity = await wallet.get(userName);
         if (!identity) {
-            console.log('An identity for the user "appUser" does not exist in the wallet');
-            console.log('Run the registerUser.js application before retrying');
+            console.log(`An identity for the user '${userName}' does not exist in the wallet`);
+            console.log(`Run the registerUser.js ${userName} application before retrying`);
             return;
         }
 
         // Create a new gateway for connecting to our peer node.
         const gateway = new Gateway();
-        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+        await gateway.connect(ccp, { wallet, identity: userName, discovery: { enabled: true, asLocalhost: true } });
 
         // Get the network (channel) our contract is deployed to.
         const network = await gateway.getNetwork('mychannel');
+        console.log("----------", network);
 
         // Get the contract from the network.
         const contract = network.getContract('ipo');
--- fabric-samples/ipo-settlement/startFabric.sh
+++ fabric-samples/ipo-settlement/startFabric.sh
@@ -23,22 +23,20 @@ else
 fi
 
 # clean out any old identites in the wallets
-rm -rf javascript/wallet/*
+rm -rf client-javascript/wallet/*
 
 # launch network; create channel and join peer to channel
 pushd ../test-network
 ./network.sh down
 ./network.sh up createChannel -ca -s couchdb
-./network.sh deployCC -ccn Ipo -ccv 1 -cci initLedger -ccl ${CC_SRC_LANGUAGE} -ccp ${CC_SRC_PATH}
+./network.sh deployCC -ccn ipo -ccv 1 -cci initLedger -ccl ${CC_SRC_LANGUAGE} -ccp ${CC_SRC_PATH}
 popd
 
 cat <<EOF
 
 Total setup execution time : $(($(date +%s) - starttime)) secs ...
 
-Next, use the FabCar applications to interact with the deployed FabCar contract.
-The FabCar applications are available in multiple programming languages.
-Follow the instructions for the programming language of your choice:
+Next, use the ipo applications to interact with the deployed ipo contract.
 
 JavaScript:
 
@@ -52,21 +50,13 @@ JavaScript:
   called appUser which will be used by the other applications to interact with the deployed
   FabCar contract:
     node enrollAdmin
-    node registerUser
+    node registerUser {userName}
 
-  You can run the invoke application as follows. By default, the invoke application will
-  create a new car, but you can update the application to submit other transactions:
+  You can run the invoke application as follows. 
     node invoke
 
   You can run the query application as follows. By default, the query application will
-  return all cars, but you can update the application to evaluate other transactions:
-    node query
-
-  The test will invoke the sample client app which perform the following:
-    - Import user credentials into the wallet (if they don't already exist there)
-    - Submit a transaction to create a new car
-    - Evaluate a transaction (query) to return details of this car
-    - Submit a transaction to change the owner of this car
-    - Evaluate a transaction (query) to return the updated details of this car
+  return all the shares, but you can update the application to evaluate other transactions:
+    node query {userName}
 
 EOF
