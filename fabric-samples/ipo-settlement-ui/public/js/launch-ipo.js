4847bf6c60064b3ab17c26944cc586b7f13c653e
fedricknishant <fednish@gmail.com>
2023-03-02 14:56:52 +0530

    added launch IPO js

---
+++ fabric-samples/ipo-settlement-ui/public/js/launch-ipo.js
@@ -0,0 +1,10 @@
+document.getElementById("basic-ipo-next").onclick = (event) => {
+    event.preventDefault()
+    document.getElementById("nav-ipo-bucket-tab").click()
+}
+
+document.getElementById("ipo-bucket-next").onclick = (event) => {
+    event.preventDefault()
+    document.getElementById("nav-investor-classification-tab").click()
+}
+
