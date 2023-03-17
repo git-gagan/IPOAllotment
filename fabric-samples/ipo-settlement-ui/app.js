da5e80461d2ca517f660d31c343197f39552035e
root <root@LAPTOP-KOQKL6FT>
2023-03-17 15:23:38 +0530

    enabled cron

--- fabric-samples/ipo-settlement-ui/app.js
+++ fabric-samples/ipo-settlement-ui/app.js
@@ -40,7 +40,7 @@ import { router } from './routers/index.js'
 import passport from "passport";
 import localPassport from './configurations/localPassport.js'
 
-// import { job } from "./utils/cron.js";
+import { job } from "./utils/cron.js";
 
 /////////////////////////////////////////
 // Express setup
